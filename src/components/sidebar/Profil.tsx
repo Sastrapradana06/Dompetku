34c1938f3bddd5f1a2d9ecb77299db5c8087da98
klimalik <klimamalik14@gmail.com>
2023-10-29 11:56:13 +0700

    merge style branch

--- src/components/sidebar/Profil.tsx
+++ src/components/sidebar/Profil.tsx
@@ -1,9 +1,12 @@
 import styles from "./sidebar.module.css";
+import Image from "next/image";
 
-export default function Profil({handleShowImg} : {handleShowImg:Function}) {
+export default function Profil({ handleShowImg }: { handleShowImg: Function }) {
   return (
     <div className={styles.profil} id="profil">
-      <div className={styles.img_user} onClick={() => handleShowImg()}></div>
+      <div className={styles.img_user} onClick={() => handleShowImg()}>
+        <Image src={'/'} alt="imgUser" width={0} height={0} />
+      </div>
       <div className={styles.username}>
         <h2>Sastra Pradana</h2>
         <p>Batu Bata</p>
