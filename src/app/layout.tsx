9d68793c379a3473a6d0f4fb6eaea69ea0400f3e
klimalik <klimamalik14@gmail.com>
2023-11-20 20:03:12 +0700

    fix bug tampilan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'BisnisKu',
+  title: 'Dompetku',
   description: 'My Bisnis',
 }
 
